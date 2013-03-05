b97dcd6223d384fb5a1c212c1ef2ee1a6d631170
araujodavid <david.brasillo@gmail.com>
2013-03-05 18:51:21 -0300

    Commit para adicionar versão 1.0 das classes collectors

---
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/MemoryCollector.java
@@ -0,0 +1,16 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector;
+
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.common.AbstractCollector;
+
+/**
+ * 
+ * @author Daivd Araújo
+ * @version 1.0
+ * @since March 5, 2013
+ * 
+ */
+
+public class MemoryCollector extends AbstractCollector {
+
+	//TODO!
+}
---
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/common/AbstractCollector.java
@@ -0,0 +1,14 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.common;
+
+/**
+ * 
+ * @author Daivd Araújo
+ * @version 1.0
+ * @since March 5, 2013
+ * 
+ */
+
+public abstract class AbstractCollector {
+
+	//TODO!
+}
