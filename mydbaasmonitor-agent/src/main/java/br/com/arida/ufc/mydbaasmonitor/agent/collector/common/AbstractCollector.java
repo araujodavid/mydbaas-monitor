33cbb76f5c1e6e1f04159d9fd8ecf74fd8efdf06
araujodavid <david.brasillo@gmail.com>
2013-03-06 09:58:10 -0300

    Commit para adicionar a herança de Thread na classe AbstractCollector e para adicionar a classe MonitoringAgent

--- mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/common/AbstractCollector.java
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/common/AbstractCollector.java
@@ -3,12 +3,12 @@ package main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.common;
 /**
  * 
  * @author Daivd Araújo
- * @version 1.0
+ * @version 1.1
  * @since March 5, 2013
  * 
  */
 
-public abstract class AbstractCollector {
+public abstract class AbstractCollector extends Thread {
 
 	//TODO!
 }
---
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/monitor/MonitoringAgent.java
@@ -0,0 +1,18 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.agent.monitor;
+
+/**
+ * 
+ * Class that will initialize the monitoring processes.
+ * @author Daivd Araújo
+ * @version 1.0
+ * @since March 6, 2013
+ * 
+ */
+
+public class MonitoringAgent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
