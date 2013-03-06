06c3ae44bc9387dbf52f56203aae6461e55d9f36
araujodavid <david.brasillo@gmail.com>
2013-03-06 11:39:52 -0300

    Commit para adicionar a classe abstrata AbstractMetric

---
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/entity/common/AbstractMetric.java
@@ -0,0 +1,13 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.common;
+
+/**
+ * 
+ * @author Daivd Araújo
+ * @version 1.0
+ * @since March 6, 2013
+ * 
+ */
+
+public abstract class AbstractMetric {
+
+}
