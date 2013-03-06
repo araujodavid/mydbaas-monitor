abd74d289d7f21531a3230dffa131d4e3b0c0b0c
araujodavid <david.brasillo@gmail.com>
2013-03-06 18:07:30 -0300

    Commit para adicionar as entidades Cpu, Memory e SO Metric

---
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/entity/SOMetric.java
@@ -0,0 +1,15 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity;
+
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.common.AbstractMetric;
+
+/**
+ * 
+ * @author Daivd Araújo
+ * @version 1.0
+ * @since March 6, 2013
+ * 
+ */
+
+public class SOMetric extends AbstractMetric {
+
+}
