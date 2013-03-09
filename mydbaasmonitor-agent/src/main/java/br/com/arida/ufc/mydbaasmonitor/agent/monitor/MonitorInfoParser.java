c79d51ff6afb0bb1fe1e60500452411beceac7fc
araujodavid <david.brasillo@gmail.com>
2013-03-09 10:19:46 -0300

    Commit para adicionar os campos e métodos a classe MonitorInfoParser

--- mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/monitor/MonitorInfoParser.java
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/monitor/MonitorInfoParser.java
@@ -1,9 +1,14 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.agent.monitor;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * 
  * @author Daivd Araújo
- * @version 1.1
+ * @version 2.0
  * @since March 5, 2013
  * 
  */
@@ -14,10 +19,18 @@ public class MonitorInfoParser {
 	private static MonitorInfoParser uniqueInstance;
 	//Unique identifier of the resource on the server
 	private Integer identifier;
-	//Time unit for the monitoring cycle (seconds)
-	private Integer monitoringCycle;
+	//User owns the resource
+	private Integer user;
+	//Geral URL
+	private String geralURL;
+	//URL to send the general system information
+	private String informationURL;
+	//Properties agent configuration file
+	private Properties properties;
 
-	private MonitorInfoParser() {}
+	private MonitorInfoParser() {
+		this.properties = new Properties();
+	}
 
 	public static MonitorInfoParser getInstance() {
 		if (uniqueInstance == null) {
@@ -26,20 +39,54 @@ public class MonitorInfoParser {
 	    return uniqueInstance;
 	}
 
-	public Integer getId() {
+	public Integer getIdentifier() {
 		return identifier;
 	}
 
-	public void setId(Integer identifier) {
+	public void setIdentifier(Integer identifier) {
 		this.identifier = identifier;
 	}
 
-	public Integer getMonitoringCycle() {
-		return monitoringCycle;
+	public Integer getUser() {
+		return user;
 	}
 
-	public void setMonitoringCycle(Integer monitoringCycle) {
-		this.monitoringCycle = monitoringCycle;
-	}	
+	public void setUser(Integer user) {
+		this.user = user;
+	}
+
+	public String getInformationURL() {
+		return informationURL;
+	}
+
+	public void setInformationURL(String informationURL) {
+		this.informationURL = informationURL;
+	}
 	
+	public String getGeralURL() {
+		return geralURL;
+	}
+
+	public void setGeralURL(String geralURL) {
+		this.geralURL = geralURL;
+	}
+
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+
+	public void loadContextFile(String path) throws FileNotFoundException, IOException{
+		this.properties.load(new FileInputStream(path));
+	}
+	
+	public void loadProperties() {
+		this.identifier = Integer.valueOf(this.properties.getProperty("identifier"));
+		this.user = Integer.valueOf(this.properties.getProperty("user"));
+		this.geralURL = this.properties.getProperty("geral.url");
+		this.informationURL = this.properties.getProperty("geral.information.url");
+	}
 }
