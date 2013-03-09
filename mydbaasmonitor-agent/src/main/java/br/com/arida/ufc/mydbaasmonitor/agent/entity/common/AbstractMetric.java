2b1e9cef9080f308cb0ebe896e3c0cfaa3633a31
araujodavid <david.brasillo@gmail.com>
2013-03-09 10:14:33 -0300

    Commit para o método abstrato loadMetricProperties a classe AbstractMetric

--- mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/entity/common/AbstractMetric.java
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/entity/common/AbstractMetric.java
@@ -1,13 +1,36 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.common;
 
+import java.util.Properties;
+
 /**
  * 
  * @author Daivd Araújo
- * @version 1.0
+ * @version 2.0
  * @since March 6, 2013
  * 
  */
 
 public abstract class AbstractMetric {
 
+	private String cyclo;
+	private String url;
+
+	public String getCyclo() {
+		return cyclo;
+	}
+
+	public void setCyclo(String cyclo) {
+		this.cyclo = cyclo;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public abstract void loadMetricProperties(Properties properties);
+	
 }
