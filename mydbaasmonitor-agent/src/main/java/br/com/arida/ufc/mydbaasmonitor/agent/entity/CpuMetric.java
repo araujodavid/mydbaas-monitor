379022e2e5929831618a64bce7b7957f0d306d4d
araujodavid <david.brasillo@gmail.com>
2013-03-09 10:17:55 -0300

    Commit para adicionar os campos e métodos a classe CpuMetric

--- mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/entity/CpuMetric.java
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/entity/CpuMetric.java
@@ -1,15 +1,75 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity;
 
+import java.util.Properties;
+
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.common.AbstractMetric;
 
 /**
  * 
  * @author Daivd Araújo
- * @version 1.0
+ * @version 2.0
  * @since March 6, 2013
  * 
  */
 
 public class CpuMetric extends AbstractMetric {
+	
+	private static CpuMetric uniqueInstance;
+	private double cpuUser;
+	private double cpuSystem;	
+	private double cpuNice;
+	private double cpuWait;
+	private double cpuIdle;
+	private double cpuCombined;
+	
+	private CpuMetric() {}
 
+	public static CpuMetric getInstance() {
+		if (uniqueInstance == null) {
+			uniqueInstance = new CpuMetric();
+	    }
+	    return uniqueInstance;
+	}
+	
+	public double getCpuUser() {
+		return cpuUser;
+	}
+	public void setCpuUser(double cpuUser) {
+		this.cpuUser = cpuUser;
+	}
+	public double getCpuSystem() {
+		return cpuSystem;
+	}
+	public void setCpuSystem(double cpuSystem) {
+		this.cpuSystem = cpuSystem;
+	}
+	public double getCpuNice() {
+		return cpuNice;
+	}
+	public void setCpuNice(double cpuNice) {
+		this.cpuNice = cpuNice;
+	}
+	public double getCpuWait() {
+		return cpuWait;
+	}
+	public void setCpuWait(double cpuWait) {
+		this.cpuWait = cpuWait;
+	}
+	public double getCpuIdle() {
+		return cpuIdle;
+	}
+	public void setCpuIdle(double cpuIdle) {
+		this.cpuIdle = cpuIdle;
+	}
+	public double getCpuCombined() {
+		return cpuCombined;
+	}
+	public void setCpuCombined(double cpuCombined) {
+		this.cpuCombined = cpuCombined;
+	}
+
+	@Override
+	public void loadMetricProperties(Properties properties) {
+		// TODO Auto-generated method stub		
+	}	
 }
