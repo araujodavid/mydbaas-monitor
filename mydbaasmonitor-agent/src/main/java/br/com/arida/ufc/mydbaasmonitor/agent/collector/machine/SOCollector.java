9321b945fdc2a804e2d60e0546ad6ec19330072a
araujodavid <david.brasillo@gmail.com>
2013-03-09 10:11:41 -0300

    Commit para implementar o método abstrato loadMetric para SOController

--- mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/machine/SOCollector.java
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/machine/SOCollector.java
@@ -1,10 +1,13 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.machine;
 
+import org.hyperic.sigar.CpuInfo;
+import org.hyperic.sigar.Mem;
+import org.hyperic.sigar.OperatingSystem;
 import org.hyperic.sigar.Sigar;
-
+import org.hyperic.sigar.SigarException;
+import org.hyperic.sigar.Swap;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.machine.common.AbstractMachineCollector;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.SOMetric;
-
 
 /**
  * 
@@ -17,9 +20,24 @@ import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.SOMetric;
 public class SOCollector extends AbstractMachineCollector<SOMetric>  {
 
 	@Override
-	public SOMetric loadMetric(Sigar sigar) {
-		// TODO Auto-generated method stub
-		return null;
+	public SOMetric loadMetric(Sigar sigar) throws SigarException {
+		SOMetric soMetric = SOMetric.getInstance();
+		OperatingSystem sys = OperatingSystem.getInstance();
+		Mem mem = sigar.getMem();
+		Swap swap = sigar.getSwap();
+		CpuInfo cpuInfo = sigar.getCpuInfoList()[0];
+		
+		soMetric.setOperatingSystem(sys.getDescription());
+		soMetric.setKernelName(sys.getName());
+		soMetric.setKernelVersion(sys.getVersion());
+		soMetric.setArchitecture(sys.getArch());
+		soMetric.setTotalMemory(mem.getTotal());
+		soMetric.setTotalSwap(swap.getTotal());
+		soMetric.setTotalCPUCores(cpuInfo.getTotalCores());
+		soMetric.setTotalCPUSockets(cpuInfo.getTotalSockets());
+		soMetric.setTotalCoresPerSocket(cpuInfo.getCoresPerSocket());
+		
+		return soMetric;
 	}
 
 	@Override
