da73057e6f6484194ad47451765ce6329bd61e3c
araujodavid <david.brasillo@gmail.com>
2013-03-09 10:10:47 -0300

    Commit para implementar o método abstrato loadMetric para MemoryController

--- mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/machine/MemoryCollector.java
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/machine/MemoryCollector.java
@@ -1,10 +1,11 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.machine;
 
+import org.hyperic.sigar.Mem;
 import org.hyperic.sigar.Sigar;
-
+import org.hyperic.sigar.SigarException;
+import org.hyperic.sigar.Swap;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.machine.common.AbstractMachineCollector;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.MemoryMetric;
-
 
 /**
  * 
@@ -16,16 +17,31 @@ import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.MemoryMetric;
 
 public class MemoryCollector extends AbstractMachineCollector<MemoryMetric>  {
 
+    private static Long format(long value) {
+        return new Long(value / 1024);
+    }
+	
 	@Override
-	public MemoryMetric loadMetric(Sigar sigar) {
-		// TODO Auto-generated method stub
-		return null;
+	public MemoryMetric loadMetric(Sigar sigar) throws SigarException {
+		MemoryMetric memoryMetric = MemoryMetric.getInstance();
+		Mem mem = sigar.getMem();
+		Swap swap = sigar.getSwap();
+		
+		memoryMetric.setSwapUsed(format(swap.getUsed()));
+		memoryMetric.setSwapFree(format(swap.getFree()));
+		memoryMetric.setMemoryUsed(format(mem.getUsed()));
+		memoryMetric.setMemoryFree(format(mem.getFree()));
+		memoryMetric.setMemoryUsedPercent(mem.getUsedPercent());
+		memoryMetric.setMemoryFreePercent(mem.getFreePercent());
+		memoryMetric.setBuffersCacheUsed(format(mem.getActualUsed()));
+		memoryMetric.setBuffersCacheFree(format(mem.getActualFree()));
+		
+		return memoryMetric;
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
 	}
 	
 }
