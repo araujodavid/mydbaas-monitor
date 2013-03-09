167737bdbde7c9eae7ecaf429b2893cb8d181ca5
araujodavid <david.brasillo@gmail.com>
2013-03-09 10:18:20 -0300

    Commit para adicionar os campos e métodos a classe MemoryMetric

--- mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/entity/MemoryMetric.java
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/entity/MemoryMetric.java
@@ -1,15 +1,105 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity;
 
+import java.util.Properties;
+
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.common.AbstractMetric;
 
 /**
  * 
  * @author Daivd Araújo
- * @version 1.0
+ * @version 2.0
  * @since March 6, 2013
  * 
  */
 
 public class MemoryMetric extends AbstractMetric {
+	
+	private static MemoryMetric uniqueInstance;
+	private long swapUsed;
+	private long swapFree;
+	private long memoryUsed;
+	private long memoryFree;
+	private double memoryUsedPercent;
+	private double memoryFreePercent;
+	private long buffersCacheUsed;
+	private long buffersCacheFree;
+	
+	private MemoryMetric() {}
 
+	public static MemoryMetric getInstance() {
+		if (uniqueInstance == null) {
+			uniqueInstance = new MemoryMetric();
+	    }
+	    return uniqueInstance;
+	}
+
+	public long getSwapUsed() {
+		return swapUsed;
+	}
+
+	public void setSwapUsed(long swapUsed) {
+		this.swapUsed = swapUsed;
+	}
+
+	public long getSwapFree() {
+		return swapFree;
+	}
+
+	public void setSwapFree(long swapFree) {
+		this.swapFree = swapFree;
+	}
+
+	public long getMemoryUsed() {
+		return memoryUsed;
+	}
+
+	public void setMemoryUsed(long memoryUsed) {
+		this.memoryUsed = memoryUsed;
+	}
+
+	public long getMemoryFree() {
+		return memoryFree;
+	}
+
+	public void setMemoryFree(long memoryFree) {
+		this.memoryFree = memoryFree;
+	}
+
+	public double getMemoryUsedPercent() {
+		return memoryUsedPercent;
+	}
+
+	public void setMemoryUsedPercent(double memoryUsedPercent) {
+		this.memoryUsedPercent = memoryUsedPercent;
+	}
+
+	public double getMemoryFreePercent() {
+		return memoryFreePercent;
+	}
+
+	public void setMemoryFreePercent(double memoryFreePercent) {
+		this.memoryFreePercent = memoryFreePercent;
+	}
+
+	public long getBuffersCacheUsed() {
+		return buffersCacheUsed;
+	}
+
+	public void setBuffersCacheUsed(long buffersCacheUsed) {
+		this.buffersCacheUsed = buffersCacheUsed;
+	}
+
+	public long getBuffersCacheFree() {
+		return buffersCacheFree;
+	}
+
+	public void setBuffersCacheFree(long buffersCacheFree) {
+		this.buffersCacheFree = buffersCacheFree;
+	}
+
+	@Override
+	public void loadMetricProperties(Properties properties) {
+		// TODO Auto-generated method stub		
+	}	
+	
 }
