0be8aa04f33645d237315f20499bd23d7eac986d
araujodavid <david.brasillo@gmail.com>
2013-03-10 13:47:50 -0300

    Commit para adicionar classe controller que receberá as requisições dos agentes sobre a máquina

---
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/receiver/MachineReceiverController.java
@@ -0,0 +1,32 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.core.controller.receiver;
+
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Resource;
+
+/**
+ * 
+ * Class that handles requests sent by the monitoring agents about machine.
+ * @author Daivd Araújo
+ * @version 1.0
+ * @since March 10, 2013
+ * 
+ */
+
+@Resource
+public class MachineReceiverController {
+	
+	public MachineReceiverController() {
+		//TODO!
+	}
+	
+	@Post("")
+	public void cpu() {
+		//TODO!
+	}
+	
+	@Post("")
+	public void memory() {
+		//TODO!
+	}
+
+}
