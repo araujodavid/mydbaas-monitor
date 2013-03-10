b3027c683f8b746a3a4d4aeb58ca705579a65629
araujodavid <david.brasillo@gmail.com>
2013-03-10 13:48:14 -0300

    Commit para adicionar classe controller que receberá as requisições dos agentes sobre o banco de dados

---
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/receiver/DatabaseReceiverController.java
@@ -0,0 +1,20 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.core.controller.receiver;
+
+import br.com.caelum.vraptor.Resource;
+
+/**
+ * 
+ * Class that handles requests sent by the monitoring agents about database.
+ * @author Daivd Araújo
+ * @version 1.0
+ * @since March 10, 2013
+ * 
+ */
+
+@Resource
+public class DatabaseReceiverController {
+
+	public DatabaseReceiverController() {
+		//TODO!
+	}
+}
