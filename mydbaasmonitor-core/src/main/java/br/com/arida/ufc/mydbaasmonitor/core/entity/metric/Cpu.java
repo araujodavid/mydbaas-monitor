f75cc95c7b4819b6754ff4b1d8157457cfc96a8f
araujodavid <david.brasillo@gmail.com>
2013-03-12 12:34:47 -0300

    Commit para adicionar diretório das classes metrics e adicionar a classe MachineMetricRepository

---
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/entity/metric/Cpu.java
@@ -0,0 +1,62 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.core.entity.metric;
+
+import main.java.br.com.arida.ufc.mydbaasmonitor.core.entity.metric.common.AbstractMetric;
+
+public class Cpu extends AbstractMetric {
+
+	private double cpuUser;
+	private double cpuSystem;	
+	private double cpuNice;
+	private double cpuWait;
+	private double cpuIdle;
+	private double cpuCombined;
+	
+	public double getCpuUser() {
+		return cpuUser;
+	}
+	
+	public void setCpuUser(double cpuUser) {
+		this.cpuUser = cpuUser;
+	}
+	
+	public double getCpuSystem() {
+		return cpuSystem;
+	}
+	
+	public void setCpuSystem(double cpuSystem) {
+		this.cpuSystem = cpuSystem;
+	}
+	
+	public double getCpuNice() {
+		return cpuNice;
+	}
+	
+	public void setCpuNice(double cpuNice) {
+		this.cpuNice = cpuNice;
+	}
+	
+	public double getCpuWait() {
+		return cpuWait;
+	}
+	
+	public void setCpuWait(double cpuWait) {
+		this.cpuWait = cpuWait;
+	}
+	
+	public double getCpuIdle() {
+		return cpuIdle;
+	}
+	
+	public void setCpuIdle(double cpuIdle) {
+		this.cpuIdle = cpuIdle;
+	}
+	
+	public double getCpuCombined() {
+		return cpuCombined;
+	}
+	
+	public void setCpuCombined(double cpuCombined) {
+		this.cpuCombined = cpuCombined;
+	}	
+
+}
---
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/repository/MachineMetricRepository.java
@@ -0,0 +1,86 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.core.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import main.java.br.com.arida.ufc.mydbaasmonitor.core.entity.metric.Cpu;
+import main.java.br.com.arida.ufc.mydbaasmonitor.core.entity.metric.Memory;
+import main.java.br.com.arida.ufc.mydbaasmonitor.core.repository.connection.Pool;
+import br.com.caelum.vraptor.ioc.Component;
+
+/**
+ * @author David Araújo
+ * @version 2.0
+ * @since March 11, 2013 
+ */
+
+@Component
+public class MachineMetricRepository {
+
+	private Connection connection = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+    
+	public boolean saveCpuMetric(Cpu cpu) {
+		try {
+			this.connection = Pool.getConnection(Pool.JDBC_MySQL);
+			this.preparedStatement = this.connection.prepareStatement(
+					"insert into cpu_metric " +
+					"(`virtual_machine`, `cpu_user`, `cpu_system`, `cpu_nice`, `cpu_wait`, `cpu_idle`, `cpu_combined`, `record_date`) " +
+					"values (?, ?, ?, ?, ?, ?, ?, ?);");
+			
+			this.preparedStatement.setInt(1, cpu.getMachine());
+			this.preparedStatement.setDouble(2, cpu.getCpuUser());
+			this.preparedStatement.setDouble(3, cpu.getCpuSystem());
+			this.preparedStatement.setDouble(4, cpu.getCpuNice());
+			this.preparedStatement.setDouble(5, cpu.getCpuWait());
+			this.preparedStatement.setDouble(6, cpu.getCpuIdle());
+			this.preparedStatement.setDouble(7, cpu.getCpuCombined());
+			this.preparedStatement.setString(8, cpu.getRecordDate());
+			
+			this.preparedStatement.executeUpdate();
+			return true;
+		} 
+		catch(SQLException se) {se.printStackTrace();}
+		catch (RuntimeException re) {re.printStackTrace();}
+		finally {
+            try { resultSet.close(); } catch(Exception e) { }
+            try { preparedStatement.close(); } catch(Exception e) { }
+            try { connection.close(); } catch(Exception e) { }
+        }
+		return false;
+	}//saveCpuMetric()
+	
+	public boolean saveMemoryMetric(Memory memory) {
+		try {
+			this.connection = Pool.getConnection(Pool.JDBC_MySQL);
+			this.preparedStatement = this.connection.prepareStatement(
+					"insert into cpu_metric " +
+					"(`virtual_machine`, `swap_used`, `swap_free`, `memory_used`, `memory_free`, `memory_used_percent`, `memory_free_percent`, `buffers_cache_used`, `buffers_cache_free`, `record_date`) " +
+					"values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+			
+			this.preparedStatement.setInt(1, memory.getMachine());
+			this.preparedStatement.setLong(2, memory.getSwapUsed());
+			this.preparedStatement.setLong(3, memory.getSwapFree());
+			this.preparedStatement.setLong(4, memory.getMemoryUsed());
+			this.preparedStatement.setLong(5, memory.getMemoryFree());
+			this.preparedStatement.setDouble(6, memory.getMemoryUsedPercent());
+			this.preparedStatement.setDouble(7, memory.getMemoryFreePercent());
+			this.preparedStatement.setLong(8, memory.getBuffersCacheUsed());
+			this.preparedStatement.setLong(9, memory.getBuffersCacheFree());
+			this.preparedStatement.setString(10, memory.getRecordDate());
+			
+			this.preparedStatement.executeUpdate();
+			return true;
+		} 
+		catch(SQLException se) {se.printStackTrace();}
+		catch (RuntimeException re) {re.printStackTrace();}
+		finally {
+            try { resultSet.close(); } catch(Exception e) { }
+            try { preparedStatement.close(); } catch(Exception e) { }
+            try { connection.close(); } catch(Exception e) { }
+        }
+		return false;
+	}//saveMemoryMetric()
+}
