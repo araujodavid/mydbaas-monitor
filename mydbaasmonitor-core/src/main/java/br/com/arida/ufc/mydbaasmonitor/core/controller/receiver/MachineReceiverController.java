912effcc623ec8620bd5dbc200e157313078ad09
araujodavid <david.brasillo@gmail.com>
2013-03-13 10:33:38 -0300

    Commit para adicionar o método information()

--- mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/receiver/MachineReceiverController.java
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/receiver/MachineReceiverController.java
@@ -1,8 +1,10 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.core.controller.receiver;
 
+import main.java.br.com.arida.ufc.mydbaasmonitor.core.entity.VirtualMachine;
 import main.java.br.com.arida.ufc.mydbaasmonitor.core.entity.metric.Cpu;
 import main.java.br.com.arida.ufc.mydbaasmonitor.core.entity.metric.Memory;
 import main.java.br.com.arida.ufc.mydbaasmonitor.core.repository.MachineMetricRepository;
+import main.java.br.com.arida.ufc.mydbaasmonitor.core.repository.VirtualMachineRepository;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.view.DefaultStatus;
@@ -11,7 +13,7 @@ import br.com.caelum.vraptor.view.DefaultStatus;
  * 
  * Class that handles requests sent by the monitoring agents about machine.
  * @author Daivd Araújo
- * @version 1.0
+ * @version 2.0
  * @since March 10, 2013
  * 
  */
@@ -19,31 +21,40 @@ import br.com.caelum.vraptor.view.DefaultStatus;
 @Resource
 public class MachineReceiverController {
 	
-	private MachineMetricRepository repository;
+	private MachineMetricRepository metricRepository;
+	private VirtualMachineRepository machineRepository;
 	private DefaultStatus status;
 	
-	public MachineReceiverController(MachineMetricRepository repository, DefaultStatus status) {
-		this.repository = repository;
+	public MachineReceiverController(MachineMetricRepository metricRepository, VirtualMachineRepository machineRepository, DefaultStatus status) {
+		this.metricRepository = metricRepository;
+		this.machineRepository = machineRepository;
 		this.status = status;
+	}
+	
+	@Post("machine/info")
+	public void information(VirtualMachine virtualMachine) {
+		if (machineRepository.updateSystemInformation(virtualMachine)) {
+			status.accepted();
+		}
 	}
 	
 	@Post("machine/cpu")
 	public void cpu(Cpu cpu) {
-		if (repository.saveCpuMetric(cpu)) {
-			status.created();
+		if (metricRepository.saveCpuMetric(cpu)) {
+			status.accepted();
 		}		
 	}
 	
 	@Post("machine/memory")
 	public void memory(Memory memory) {
-		if (repository.saveMemoryMetric(memory)) {
-			status.created();
+		if (metricRepository.saveMemoryMetric(memory)) {
+			status.accepted();
 		}
 	}
 	
 	@Post("machine/disk")
 	public void disk() {
 		//TODO!
-	}
+	}	
 
 }
