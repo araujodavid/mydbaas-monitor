25eb50870ecffd632c7091c8cc0d3388b9f72a9e
araujodavid <david.brasillo@gmail.com>
2013-03-13 10:32:04 -0300

    Commit para adicionar coletor SOCollector

--- mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/monitor/MonitoringAgent.java
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/monitor/MonitoringAgent.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import java.util.Timer;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.machine.CpuCollector;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.machine.MemoryCollector;
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.machine.SOCollector;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.CpuMetric;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.MemoryMetric;
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.SOMetric;
 
 /**
  * 
@@ -30,9 +32,16 @@ public class MonitoringAgent {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		//Loads the general information of the monitoring agent
 		parser.loadProperties();
-		//
+		
+		//Collects information about the system and physical resources
+		SOMetric soMetric = SOMetric.getInstance();
+		soMetric.loadMetricProperties(parser.getProperties());		
+		SOCollector soCollector = new SOCollector(parser.getIdentifier());
+		soCollector.run();
+		
+		//Monitoring CPU
 		if (parser.getProperties().getProperty("cpu.url") != null && !parser.getProperties().getProperty("cpu.url").equals("")) {
 			CpuMetric cpuMetric = CpuMetric.getInstance();
 			cpuMetric.loadMetricProperties(parser.getProperties());
@@ -41,7 +50,7 @@ public class MonitoringAgent {
 			cpuTimer.scheduleAtFixedRate(cpuCollector, 0, 1*cpuMetric.getCyclo()*1000);
 			
 		}
-		//
+		//Monitoring Memory
 		if (parser.getProperties().getProperty("mem.url") != null && !parser.getProperties().getProperty("mem.url").equals("")) {
 			MemoryMetric memoryMetric = MemoryMetric.getInstance();
 			memoryMetric.loadMetricProperties(parser.getProperties());
