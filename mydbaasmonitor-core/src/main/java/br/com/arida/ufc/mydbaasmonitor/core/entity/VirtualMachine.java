228e5fab47ff7bb4f9721bd73dc0109a20aeda55
araujodavid <david.brasillo@gmail.com>
2013-03-18 12:52:35 -0300

    Commit para substituir campos pelo objeto do módulo common

--- mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/entity/VirtualMachine.java
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/entity/VirtualMachine.java
@@ -1,10 +1,12 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.core.entity;
 
+import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.OperatingSystem;
 import main.java.br.com.arida.ufc.mydbaasmonitor.core.entity.common.GenericResource;
 
 /**
  * 
  * @author David Araújo
+ * @version 3.0
  * @since March 4, 2013
  *
  */
@@ -13,15 +15,7 @@ public class VirtualMachine extends GenericResource {
 	
 	//key file if necessary for access
 	private String key;
-	private String operatingSystem;
-	private String kernelName;
-	private String kernelVersion;
-	private String architecture;
-	private long totalMemory;
-	private long totalSwap;
-	private int totalCPUCores;
-	private int totalCPUSockets;
-	private int totalCoresPerSocket;
+	private OperatingSystem system;
 	
 	public String getKey() {
 		return key;
@@ -29,78 +23,14 @@ public class VirtualMachine extends GenericResource {
 
 	public void setKey(String key) {
 		this.key = key;
+	}		
+	
+	public OperatingSystem getSystem() {
+		return system;
 	}
 
-	public String getOperatingSystem() {
-		return operatingSystem;
-	}
-
-	public void setOperatingSystem(String operatingSystem) {
-		this.operatingSystem = operatingSystem;
-	}
-
-	public String getKernelName() {
-		return kernelName;
-	}
-
-	public void setKernelName(String kernelName) {
-		this.kernelName = kernelName;
-	}
-
-	public String getKernelVersion() {
-		return kernelVersion;
-	}
-
-	public void setKernelVersion(String kernelVersion) {
-		this.kernelVersion = kernelVersion;
-	}
-
-	public String getArchitecture() {
-		return architecture;
-	}
-
-	public void setArchitecture(String architecture) {
-		this.architecture = architecture;
-	}
-
-	public long getTotalMemory() {
-		return totalMemory;
-	}
-
-	public void setTotalMemory(long totalMemory) {
-		this.totalMemory = totalMemory;
-	}
-
-	public long getTotalSwap() {
-		return totalSwap;
-	}
-
-	public void setTotalSwap(long totalSwap) {
-		this.totalSwap = totalSwap;
-	}
-
-	public int getTotalCPUCores() {
-		return totalCPUCores;
-	}
-
-	public void setTotalCPUCores(int totalCPUCores) {
-		this.totalCPUCores = totalCPUCores;
-	}
-
-	public int getTotalCPUSockets() {
-		return totalCPUSockets;
-	}
-
-	public void setTotalCPUSockets(int totalCPUSockets) {
-		this.totalCPUSockets = totalCPUSockets;
-	}
-
-	public int getTotalCoresPerSocket() {
-		return totalCoresPerSocket;
-	}
-
-	public void setTotalCoresPerSocket(int totalCoresPerSocket) {
-		this.totalCoresPerSocket = totalCoresPerSocket;
+	public void setSystem(OperatingSystem system) {
+		this.system = system;
 	}
 
 	@Override
