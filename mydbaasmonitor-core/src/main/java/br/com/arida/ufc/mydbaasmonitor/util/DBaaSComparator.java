fc0ed48dbc0e5cb116f0821743f72ecb4165559f
araujodavid <david.brasillo@gmail.com>
2013-03-21 16:35:17 -0300

    Commit para adicionar classe de comparação de DBaaS

---
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/util/DBaaSComparator.java
@@ -0,0 +1,27 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.util;
+
+import java.util.Comparator;
+import main.java.br.com.arida.ufc.mydbaasmonitor.core.entity.DBaaS;
+
+/**
+ * Class to compare a DBaaS.
+ * @author David Araújo
+ * @version 1.0
+ * @since March 21, 2013
+ * 
+ */
+
+public class DBaaSComparator implements Comparator<DBaaS> {
+
+	@Override
+	public int compare(DBaaS firstDBaaS, DBaaS secondDBaaS) {
+		if (firstDBaaS.getMachines().size() == secondDBaaS.getMachines().size()) {
+			return 0;
+		} else if (firstDBaaS.getMachines().size() > secondDBaaS.getMachines().size()) {
+			return 1;
+		} else {
+			return -1;
+		}		
+	}
+
+}
