a6bb647d41691a3ce8005c9ba2d9bc019fd067d8
araujodavid <david.brasillo@gmail.com>
2013-03-21 16:44:24 -0300

    Commit para adicionar o campo machines

--- mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/entity/DBaaS.java
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/entity/DBaaS.java
@@ -1,11 +1,12 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.core.entity;
 
+import java.util.List;
 import main.java.br.com.arida.ufc.mydbaasmonitor.core.entity.common.AbstractEntity;
 
 /** 
  * 
  * @author David Araújo
- * @version 1.0
+ * @version 2.0
  * @since March 4, 2013
  * 
  */
@@ -16,6 +17,8 @@ public class DBaaS extends AbstractEntity {
 	private String alias;
 	//date of registration of the resource
 	private String recordDate;
+	//list of virtual machines
+	private List<VirtualMachine> machines;
 		
 	public String getAlias() {
 		return alias;
@@ -32,10 +35,17 @@ public class DBaaS extends AbstractEntity {
 	public void setRecordDate(String recordDate) {
 		this.recordDate = recordDate;
 	}
+	
+	public List<VirtualMachine> getMachines() {
+		return machines;
+	}
+
+	public void setMachines(List<VirtualMachine> machines) {
+		this.machines = machines;
+	}
 
 	@Override
 	public String toString() {
 		return "dbaas";
-	}
-	
+	}	
 }
