95ae04e3f82456dabfd0ce4eacbbbf942b4b0b53
araujodavid <david.brasillo@gmail.com>
2013-03-25 20:17:17 -0300

    Commit para adicionar a classe que irá gerenciar conexões com banco de dados

---
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/util/DatabaseConnection.java
@@ -0,0 +1,12 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.agent.util;
+
+/**
+ * 
+ * @author Daivd Araújo
+ * @version 1.0
+ * @since March 25, 2013
+ * 
+ */
+public class DatabaseConnection {
+
+}
