ec6218c115fea606c659912b2281f2edbec251a9
araujodavid <david.brasillo@gmail.com>
2013-03-26 14:38:23 -0300

    Commit para atualizar extends e adicionar nova implementação - DiskCollector

--- mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/machine/DiskCollector.java
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/machine/DiskCollector.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -14,7 +13,7 @@ import org.hyperic.sigar.FileSystem;
 import org.hyperic.sigar.FileSystemUsage;
 import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
-import main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.machine.common.AbstractMachineCollector;
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.common.AbstractCollector;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.DiskMetric;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.server.SendResquest;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.util.DateUtil;
@@ -22,19 +21,20 @@ import main.java.br.com.arida.ufc.mydbaasmonitor.agent.util.DateUtil;
 /**
  * 
  * @author Daivd Araújo
- * @version 2.0
+ * @version 3.0
  * @since March 13, 2013
  * 
  */
 
-public class DiskCollector extends AbstractMachineCollector<DiskMetric> {
+public class DiskCollector extends AbstractCollector<DiskMetric> {
 
 	public DiskCollector(int identifier) {
 		this.machine = identifier;
 	}
 	
 	@Override
-	public void loadMetric(Sigar sigar) throws SigarException {
+	public void loadMetric(Object[] args) throws SigarException {
+		Sigar sigar = (Sigar) args[0];
 		this.metric = DiskMetric.getInstance();
 		long diskTotal = 0;
 		long diskUsed = 0;
@@ -45,7 +45,7 @@ public class DiskCollector extends AbstractMachineCollector<DiskMetric> {
 		long diskWrites = 0;
 		FileSystem[] fileSystemList = sigar.getFileSystemList();
 		FileSystemUsage fileSystemUsage;
-		
+
 		for (FileSystem fileSystem : fileSystemList) {
 			fileSystemUsage = sigar.getFileSystemUsage(fileSystem.getDirName());
 			diskTotal = diskTotal + fileSystemUsage.getTotal();
@@ -56,7 +56,7 @@ public class DiskCollector extends AbstractMachineCollector<DiskMetric> {
 			diskReads = diskReads + fileSystemUsage.getDiskReads();
 			diskWrites = diskWrites + fileSystemUsage.getDiskWrites();
 		}
-		
+
 		this.metric.setDiskFree(diskFree);
 		this.metric.setDiskUsed(diskUsed);
 		this.metric.setDiskTotal(diskTotal);
@@ -69,14 +69,13 @@ public class DiskCollector extends AbstractMachineCollector<DiskMetric> {
 	@Override
 	public void run() {
 		Sigar sigar = new Sigar();
+		//Collecting metrics
 		try {
-			//Collecting metrics
-			this.loadMetric(sigar);
-		} catch (SigarException e) {
-			// TODO Auto-generated catch block
+			this.loadMetric(new Object[] {sigar});
+		} catch (SigarException e2) {
 			System.out.println("Problem loading the Disk metric values (Sigar)");
-			e.printStackTrace();
-		}
+			e2.printStackTrace();
+		}	
 		
 		//Setting the parameters of the POST request
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
