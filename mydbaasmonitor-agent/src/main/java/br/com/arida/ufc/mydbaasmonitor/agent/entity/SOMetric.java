f582fb86a864dcb9ba1a1a58a3e493309de326e0
araujodavid <david.brasillo@gmail.com>
2013-03-27 14:19:52 -0300

    Commit para deletar as classes SOCollector, SOMetric e OperatingSystem

--- mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/entity/SOMetric.java
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/entity/SOMetric.java
@@ -1,33 +1,5 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity;
 
-import java.util.Properties;
-import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.common.LoadMetric;
-import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.OperatingSystem;
+public class SOMetric {
 
-/**
- * 
- * @author Daivd Araújo
- * @version 3.0
- * @since March 6, 2013
- * 
- */
-
-public class SOMetric extends OperatingSystem implements LoadMetric {
-
-	private static SOMetric uniqueInstance;
-	
-	private SOMetric() {}
-
-	public static SOMetric getInstance() {
-		if (uniqueInstance == null) {
-			uniqueInstance = new SOMetric();
-	    }
-	    return uniqueInstance;
-	}
-
-	@Override
-	public void loadMetricProperties(Properties properties) {
-		this.setUrl(properties.getProperty("geral.url")+properties.getProperty("information.url"));		
-	}	
-	
 }
