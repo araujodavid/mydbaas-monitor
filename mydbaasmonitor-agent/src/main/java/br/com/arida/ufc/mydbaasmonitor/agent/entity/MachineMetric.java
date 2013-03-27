8d272ad9f40336f374a0ca1b26b5382f90dce7e0
araujodavid <david.brasillo@gmail.com>
2013-03-27 14:05:17 -0300

    Commit para imports do módulo common nas classes entity

---
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/entity/MachineMetric.java
@@ -0,0 +1,33 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity;
+
+import java.util.Properties;
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.common.LoadMetric;
+import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.Machine;
+
+/**
+ * 
+ * @author Daivd Araújo
+ * @version 3.0
+ * @since March 6, 2013
+ * 
+ */
+
+public class MachineMetric extends Machine implements LoadMetric {
+
+	private static MachineMetric uniqueInstance;
+	
+	private MachineMetric() {}
+
+	public static MachineMetric getInstance() {
+		if (uniqueInstance == null) {
+			uniqueInstance = new MachineMetric();
+	    }
+	    return uniqueInstance;
+	}
+
+	@Override
+	public void loadMetricProperties(Properties properties) {
+		this.setUrl(properties.getProperty("geral.url")+properties.getProperty("information.url"));		
+	}	
+	
+}
