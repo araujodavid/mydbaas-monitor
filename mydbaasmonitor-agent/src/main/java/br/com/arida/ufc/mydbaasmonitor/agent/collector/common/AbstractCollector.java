d0bf62a754f2628cb7d74234b9ffa34a4973356c
araujodavid <david.brasillo@gmail.com>
2013-03-27 11:07:36 -0300

    Commit para adicionar o método sendMetric()

--- mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/common/AbstractCollector.java
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/common/AbstractCollector.java
@@ -1,5 +1,6 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.common;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -8,9 +9,14 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.TimerTask;
+
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.message.BasicNameValuePair;
 import com.sun.xml.internal.ws.util.StringUtils;
+
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.server.SendResquest;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.util.DateUtil;
 import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.common.AbstractMetric;
 
@@ -92,5 +98,19 @@ public abstract class AbstractCollector<T extends AbstractMetric> extends TimerT
 	 */
 	public void logMetric(Date recordDate) {
 		// TODO Auto-generated method stub		
+	}
+	
+	/**
+	 * Method to send the collected metrics to the server.
+	 * @param parameters - list of measures of the metric
+	 * @param charsetEncoding 
+	 * @return an object http response
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
+	public HttpResponse sendMetric(List<NameValuePair> parameters, String charsetEncoding) throws ClientProtocolException, IOException {
+		HttpResponse response;
+		response = SendResquest.postRequest(metric.getUrl(), parameters, charsetEncoding);
+		return response;		
 	}
 }
