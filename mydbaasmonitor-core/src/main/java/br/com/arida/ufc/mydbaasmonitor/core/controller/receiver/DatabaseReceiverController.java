ba4cc1b6ff9744292d37f37fad7abf969f0f0ee7
araujodavid <david.brasillo@gmail.com>
2013-03-29 23:23:44 -0300

    Commit para atualizar classe. Adicionar extends AbstractReceiver

--- mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/receiver/DatabaseReceiverController.java
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/receiver/DatabaseReceiverController.java
@@ -1,20 +1,26 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.core.controller.receiver;
 
+import main.java.br.com.arida.ufc.mydbaasmonitor.core.controller.receiver.common.AbstractReceiver;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.view.DefaultStatus;
 
 /**
- * 
  * Class that handles requests sent by the monitoring agents about database.
  * @author Daivd Araújo
  * @version 1.0
  * @since March 10, 2013
- * 
  */
 
 @Resource
-public class DatabaseReceiverController {
+public class DatabaseReceiverController extends AbstractReceiver {
 
-	public DatabaseReceiverController() {
-		//TODO!
+	/**
+	 * Constructor
+	 * @param status
+	 */
+	public DatabaseReceiverController(DefaultStatus status) {
+		super(status);
 	}
+
+	
 }
