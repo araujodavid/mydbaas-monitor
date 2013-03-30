1ae1b1e0377ad2bf3d851ea153d416ccb060727e
araujodavid <david.brasillo@gmail.com>
2013-03-29 23:24:27 -0300

    Commit para atualizar classe. Adicionar extends AbstractReceiver e atualizar construtor

--- mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/receiver/MachineReceiverController.java
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/receiver/MachineReceiverController.java
@@ -3,6 +3,7 @@ package main.java.br.com.arida.ufc.mydbaasmonitor.core.controller.receiver;
 import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.Cpu;
 import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.Machine;
 import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.Memory;
+import main.java.br.com.arida.ufc.mydbaasmonitor.core.controller.receiver.common.AbstractReceiver;
 import main.java.br.com.arida.ufc.mydbaasmonitor.core.repository.MachineMetricRepository;
 import main.java.br.com.arida.ufc.mydbaasmonitor.core.repository.VirtualMachineRepository;
 import br.com.caelum.vraptor.Post;
@@ -10,26 +11,29 @@ import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.view.DefaultStatus;
 
 /**
- * 
  * Class that handles requests sent by the monitoring agents about machine.
  * @author Daivd Araújo
  * @version 3.0
- * @since March 10, 2013
- * 
+ * @since March 10, 2013 
  */
 
 @Resource
-public class MachineReceiverController {
+public class MachineReceiverController extends AbstractReceiver {
 	
 	private MachineMetricRepository metricRepository;
 	private VirtualMachineRepository machineRepository;
-	private DefaultStatus status;
 	
-	public MachineReceiverController(MachineMetricRepository metricRepository, VirtualMachineRepository machineRepository, DefaultStatus status) {
+	/**
+	 * Constructor
+	 * @param status
+	 * @param metricRepository
+	 * @param machineRepository
+	 */
+	public MachineReceiverController(DefaultStatus status, MachineMetricRepository metricRepository, VirtualMachineRepository machineRepository) {
+		super(status);
 		this.metricRepository = metricRepository;
 		this.machineRepository = machineRepository;
-		this.status = status;
-	}	
+	}
 
 	/**
 	 * Method that receives information about the operating system and machine settings.
@@ -43,9 +47,11 @@ public class MachineReceiverController {
 		}
 	}
 	
-	// To create a new method receiver is necessary to call the object "metric". 
-	// Add the parameters: machine (int) and recordDate (String).
-	// The monitoring agents will send the data in this format.
+	/*
+	 * To create a new method receiver is necessary to call the object "metric". 
+     * Add the parameters: machine (int) and recordDate (String).
+     * The monitoring agents will send the data in this format.
+	 */
 	
 	/**
 	 * Method that receives the collection of CPU.
