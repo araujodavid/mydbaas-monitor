dec1153964ebe3776463a08dc24106ec070876b2
araujodavid <david.brasillo@gmail.com>
2013-04-01 12:13:01 -0300

    Commit para adicionar implementação do método getMetricFields()

--- mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/repository/MetricRepository.java
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/repository/MetricRepository.java
@@ -2,6 +2,7 @@ package main.java.br.com.arida.ufc.mydbaasmonitor.core.repository;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import br.com.caelum.vraptor.ioc.Component;
 
@@ -16,30 +17,36 @@ public class MetricRepository {
 
 	/**
 	 * Method to save a metric in the database
-	 * @param metric - given metric
+	 * @param metric - given metric object
 	 * @param machine
 	 * @param recordDate
 	 * @return true if the metric is saved
 	 */
-	public boolean saveMetric(Object metric, int machine, String recordDate) {
-		List<Field> fields = new ArrayList<Field>();
-		
-		//Gets the class of the metric
-		Class<?> clazz = metric.getClass();
+	public boolean saveMetric(Object metric, int machine, String recordDate) {		
 		return true;
 	}//saveMetric()
 	
 	/**
 	 * Method to create a table in the database for a given metric
-	 * @param metric - given metric
+	 * @param metric - given metric object
 	 * @return true if the table is created
 	 */
 	public boolean createMetricTable(Object metric) {
 		return true;
-	}
+	}//createMetricTable()
 	
-	
-	public List<Field> getMetricFields(Object metric) {		
-		return null;
-	}
+	/**
+	 * Method to recover the fields of a given metric
+	 * @param metric - given metric object
+	 * @return the list of class fields
+	 */
+	public List<Field> getMetricFields(Object metric) {
+		List<Field> fields = new ArrayList<Field>();		
+		//Gets the class of the metric
+		Class<?> clazz = metric.getClass();		
+		//Gets fields from the class
+		fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+		
+		return fields;
+	}//getMetricFields()
 }
