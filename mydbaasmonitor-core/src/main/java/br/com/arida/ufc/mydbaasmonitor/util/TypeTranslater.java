02040c46edebc3db1ba7885881349d092ea89887
araujodavid <david.brasillo@gmail.com>
2013-04-02 11:12:58 -0300

    Commit para adicionar classe util TypeTranslater

---
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/util/TypeTranslater.java
@@ -0,0 +1,16 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.util;
+
+/**
+ * Class to translate the Java types to SQL types.
+ * @author David Araújo
+ * @version 2
+ * @since April 2, 2013 
+ */
+public class TypeTranslater {
+
+	
+	public static String getSQLType(Class<?> typeClass) {
+		//TODO
+		return null;		
+	}
+}
