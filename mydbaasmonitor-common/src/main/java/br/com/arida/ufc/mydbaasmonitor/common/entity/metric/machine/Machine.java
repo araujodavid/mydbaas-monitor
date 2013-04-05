97d3e2493d87f504792b7164f9513d74d18e7d6d
araujodavid <david.brasillo@gmail.com>
2013-04-05 00:25:02 -0300

    Commit para alterar diretório

--- mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/metric/machine/Machine.java
+++ mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/metric/machine/Machine.java
@@ -1,4 +1,4 @@
-package main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric;
+package main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.machine;
 
 import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.common.AbstractMetric;
 
