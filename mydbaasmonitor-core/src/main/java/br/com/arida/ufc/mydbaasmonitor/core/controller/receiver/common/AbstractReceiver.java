36facf2cac1848edc73486fda6b74d91034abe0c
araujodavid <david.brasillo@gmail.com>
2013-04-11 00:01:49 -0300

    Commit para adicionar novo campo e atualizar o método construtor da classe AbstractReceiver

--- mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/receiver/common/AbstractReceiver.java
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/receiver/common/AbstractReceiver.java
@@ -1,22 +1,25 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.core.controller.receiver.common;
 
+import main.java.br.com.arida.ufc.mydbaasmonitor.core.repository.MetricRepository;
 import br.com.caelum.vraptor.view.DefaultStatus;
 
 /**
  * Abstract class for new Receivers
  * @author David Araújo
- * @version 1.0
+ * @version 2.0
  * @since March 29, 2013
  */
 public abstract class AbstractReceiver {
 
 	protected DefaultStatus status;
+	protected MetricRepository repository;
 	
 	/**
 	 * Default constructor for Receiver
 	 * @param status
 	 */
-	public AbstractReceiver(DefaultStatus status) {
+	public AbstractReceiver(DefaultStatus status, MetricRepository repository) {
 		this.status = status;
+		this.repository = repository;
 	}
 }
