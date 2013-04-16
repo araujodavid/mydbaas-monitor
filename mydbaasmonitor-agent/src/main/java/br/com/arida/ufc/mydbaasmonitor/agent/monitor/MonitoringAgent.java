e9df03ac356d8842682d66debb811d09f1bafd19
araujodavid <david.brasillo@gmail.com>
2013-04-16 14:40:05 -0300

    Commit para adicionar implementação do método startCollectors() - MonitoringAgent

--- mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/monitor/MonitoringAgent.java
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/monitor/MonitoringAgent.java
@@ -2,12 +2,14 @@ package main.java.br.com.arida.ufc.mydbaasmonitor.agent.monitor;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Timer;
+import java.util.TimerTask;
 import com.sun.xml.internal.ws.util.StringUtils;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.machine.CpuCollector;
 import main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.machine.MachineCollector;
@@ -17,16 +19,19 @@ import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.MachineMetric;
 /**
  * 
  * Class that will initialize the monitoring processes.
- * @author Daivd Araújo
- * @version 3.0
+ * @author Daivd Araújo - @araujodavid
+ * @version 4.0
  * @since March 6, 2013
  * 
  */
 
 public class MonitoringAgent {
 	
+	private List<Timer> timers;
+	private List<Object> collectors;
+	
 	/**
-	 * Method to check the metrics enabled in the config file
+	 * Method to check the metrics enabled in the config file and creates the objects
 	 * @param properties
 	 * @return a list with the objects of the metrics enabled
 	 * @throws ClassNotFoundException 
@@ -52,11 +57,25 @@ public class MonitoringAgent {
 		return enabledMetrics;
 	}//getEnabledMetrics()
 	
-	public void startCollectors(List<Object> enabledMetrics) {
+	public void startCollectors(List<Object> enabledMetrics, int identifier) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException, InstantiationException {
+		this.timers = new ArrayList<Timer>();
+		this.collectors = new ArrayList<Object>();
 		for (Object object : enabledMetrics) {
-			//For each metric do:
-			//TODO - create timer
-			//TODO - create a collector metric
+			//
+			Method getCyclo = object.getClass().getSuperclass().getSuperclass().getDeclaredMethod("getCyclo");
+			int cyclo = (int) getCyclo.invoke(object, null);
+			//
+			String collectorName = StringUtils.capitalize(object.getClass().getSimpleName().replace("Metric", "")).concat("Collector");
+			Class<?> collectorClass = Class.forName("main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector."+object.toString()+"."+collectorName);
+			Constructor constructor = collectorClass.getConstructors()[0];
+			Object objectCollector = constructor.newInstance(new Object[] {identifier});
+			//
+			Timer timer = new Timer();			
+			timer.scheduleAtFixedRate((TimerTask) objectCollector, 0, 1*cyclo*1000);
+			//
+			this.timers.add(timer);
+			//
+			this.collectors.add(objectCollector);
 		}		
 	}
 
@@ -86,7 +105,7 @@ public class MonitoringAgent {
 			cpuMetric.loadMetricProperties(parser.getProperties());
 			Timer cpuTimer = new Timer();
 			CpuCollector cpuCollector = new CpuCollector(parser.getIdentifier());
-			cpuTimer.scheduleAtFixedRate(cpuCollector, 0, 1*cpuMetric.getCyclo()*1000);			
+			cpuTimer.scheduleAtFixedRate(cpuCollector, 0, 1*cpuMetric.getCyclo()*1000);
 		}
 		
 		//TODO - Criar o loop que vai instânciar com collectors de forma automática com base no método getEnabledMetrics
