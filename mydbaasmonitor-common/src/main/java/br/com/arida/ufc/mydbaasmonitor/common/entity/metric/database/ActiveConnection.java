15a0bf855f83b0786ec6b6c82fa574f0dbd1ee7a
araujodavid <david.brasillo@gmail.com>
2013-04-17 09:26:27 -0300

    Commit para adicionar a classe da métrica ActiveConnection no módulo common

---
+++ mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/metric/database/ActiveConnection.java
@@ -0,0 +1,13 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.database;
+
+/**
+ * 
+ * @author David Araújo - @araujodavid
+ * @version 1.0
+ * @since April 17, 2013
+ *
+ */
+public class ActiveConnection {
+
+	//TODO
+}
