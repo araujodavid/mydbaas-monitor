cbab51b7a3447e5e89b7206e6c609aabcb447c8e
araujodavid <david.brasillo@gmail.com>
2013-04-22 22:15:56 -0300

    Commit para adicionar o recurso Host ao módulo common

---
+++ mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/resource/Host.java
@@ -0,0 +1,12 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.resource;
+
+/**
+ * 
+ * @author David Araújo - @araujodavid
+ * @version 1.0
+ * @since March 22, 2013
+ *
+ */
+public class Host {
+
+}
