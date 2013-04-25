f988f83cec2132899c25b66839496f437f3cf1ed
araujodavid <david.brasillo@gmail.com>
2013-04-25 17:52:09 -0300

    Commit para atualizar os comentários e adicionar o método receiver para a métrica ActiveConnection - DatabaseReceiverController

--- mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/receiver/DatabaseReceiverController.java
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/receiver/DatabaseReceiverController.java
@@ -1,8 +1,11 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.core.controller.receiver;
 
+import java.lang.reflect.InvocationTargetException;
+import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.database.ActiveConnection;
 import main.java.br.com.arida.ufc.mydbaasmonitor.core.controller.receiver.common.AbstractReceiver;
 import main.java.br.com.arida.ufc.mydbaasmonitor.core.repository.MetricRepository;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.view.DefaultStatus;
 
@@ -20,10 +23,27 @@ public class DatabaseReceiverController extends AbstractReceiver {
 	/**
 	 * Constructor
 	 * @param status
+	 * @param metricRepository
 	 */
 	public DatabaseReceiverController(DefaultStatus status, MetricRepository repository) {
 		super(status, repository);
 	}
 
-	
+	@Post("/activeconnections")
+	public void activeConnections(ActiveConnection metric, int dbms, int database, String recordDate) {
+		try {
+			if (repository.saveMetric(metric, 0, recordDate, dbms, database)) {
+				status.accepted();
+			}
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
 }
