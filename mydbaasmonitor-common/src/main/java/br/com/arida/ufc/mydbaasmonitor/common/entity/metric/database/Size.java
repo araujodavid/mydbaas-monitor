862a95334608d7c351bf5cd365ea368e66f849ea
araujodavid <david.brasillo@gmail.com>
2013-04-26 16:08:17 -0300

    Commit para adicionar a classe Size no módulo common

---
+++ mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/metric/database/Size.java
@@ -0,0 +1,19 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.database;
+
+import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.common.AbstractDatabaseMetric;
+
+/**
+ * 
+ * @author David Araújo - @araujodavid
+ * @version 1.0
+ * @since April 26, 2013
+ *
+ */
+public class Size extends AbstractDatabaseMetric {
+
+	@Override
+	public String toString() {
+		return "database";
+	}
+
+}
