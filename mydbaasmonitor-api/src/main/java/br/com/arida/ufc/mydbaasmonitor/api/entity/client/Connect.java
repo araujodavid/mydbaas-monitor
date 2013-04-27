27a3328ba40f3700330f50a5773e567b52173ecb
araujodavid <david.brasillo@gmail.com>
2013-04-26 22:19:40 -0300

    Commit para adicionar a classe Connect - módulo api

---
+++ mydbaasmonitor-api/src/main/java/br/com/arida/ufc/mydbaasmonitor/api/entity/client/Connect.java
@@ -0,0 +1,10 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.api.entity.client;
+
+/**
+ * @author Daivd Araújo - @araujodavid
+ * @version 1.0
+ * @since April 26, 2013
+ */
+public class Connect {
+
+}
