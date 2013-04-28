54fd684bdea9b15fe2415b21d4ed8b70bc165242
araujodavid <david.brasillo@gmail.com>
2013-04-28 18:58:26 -0300

    Commit para atualizar a classe MyDBaaSMonitorClient - módulo api

--- mydbaasmonitor-api/src/main/java/br/com/arida/ufc/mydbaasmonitor/api/entity/client/MyDBaaSMonitorClient.java
+++ mydbaasmonitor-api/src/main/java/br/com/arida/ufc/mydbaasmonitor/api/entity/client/MyDBaaSMonitorClient.java
@@ -1,7 +1,7 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.api.entity.client;
 
 /**
- * 
+ * Class that handles the access information to the server.
  * @author Daivd Araújo - @araujodavid
  * @version 1.0
  * @since April 28, 2013
@@ -9,7 +9,24 @@ package main.java.br.com.arida.ufc.mydbaasmonitor.api.entity.client;
 
 public class MyDBaaSMonitorClient {
 
-	public MyDBaaSMonitorClient() {
-		//TODO
+	private String serverUrl;
+	
+	/**
+	 * Default constructor
+	 * @param serverUrl
+	 */
+	public MyDBaaSMonitorClient(String serverUrl) {
+		this.serverUrl = serverUrl;
 	}
+
+	//Getters and Setters
+	
+	public String getServerUrl() {
+		return serverUrl;
+	}
+
+	public void setServerUrl(String serverUrl) {
+		this.serverUrl = serverUrl;
+	}	
+	
 }
