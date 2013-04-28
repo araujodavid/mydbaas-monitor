7bf9749bbc56b58312362213e40842decf66b071
araujodavid <david.brasillo@gmail.com>
2013-04-28 15:39:36 -0300

    Commit para adicionar classe da métrica HostInfo - módulo common

---
+++ mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/metric/host/HostInfo.java
@@ -0,0 +1,19 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.host;
+
+import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.common.AbstractMetric;
+
+/**
+ * 
+ * @author Daivd Araújo - @araujodavid
+ * @version 1.0
+ * @since April 28, 2013
+ * 
+ */
+public class HostInfo extends AbstractMetric {
+
+	@Override
+	public String toString() {
+		return "host";
+	}
+
+}
