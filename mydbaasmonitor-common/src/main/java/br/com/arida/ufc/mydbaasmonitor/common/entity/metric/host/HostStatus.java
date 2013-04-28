d9e8ecc69a03dd9cdfd6f26116b3d497ac89e67c
araujodavid <david.brasillo@gmail.com>
2013-04-28 17:20:36 -0300

    Commit para adicionar classe da métrica HostStatus - módulo common

---
+++ mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/metric/host/HostStatus.java
@@ -0,0 +1,19 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.host;
+
+import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.common.AbstractMetric;
+
+/**
+ * 
+ * @author Daivd Araújo - @araujodavid
+ * @version 1.0
+ * @since April 28, 2013
+ * 
+ */
+public class HostStatus extends AbstractMetric {
+
+	@Override
+	public String toString() {
+		return "host";
+	}
+
+}
