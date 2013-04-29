6bfa8278252682227ddc7187fd988c41fd19d62f
araujodavid <david.brasillo@gmail.com>
2013-04-29 18:15:22 -0300

    Commit para atualizar os campos da classe DomainInfo

--- mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/metric/host/DomainInfo.java
+++ mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/metric/host/DomainInfo.java
@@ -5,11 +5,39 @@ import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.common.Abs
 /**
  * 
  * @author Daivd Araújo - @araujodavid
- * @version 1.0
+ * @version 2.0
  * @since April 29, 2013
  * 
  */
 public class DomainInfo extends AbstractMetric {
+
+	private String domainInfoName;
+	private double domainInfoMemory;
+	private double domainInfoCpus;	
+	
+	public String getDomainInfoName() {
+		return domainInfoName;
+	}
+
+	public void setDomainInfoName(String domainInfoName) {
+		this.domainInfoName = domainInfoName;
+	}
+
+	public double getDomainInfoMemory() {
+		return domainInfoMemory;
+	}
+
+	public void setDomainInfoMemory(double domainInfoMemory) {
+		this.domainInfoMemory = domainInfoMemory;
+	}
+
+	public double getDomainInfoCpus() {
+		return domainInfoCpus;
+	}
+
+	public void setDomainInfoCpus(double domainInfoCpus) {
+		this.domainInfoCpus = domainInfoCpus;
+	}
 
 	@Override
 	public String toString() {
