0a0e453dc01d3708cce08450bb1fc559f4e87026
araujodavid <david.brasillo@gmail.com>
2013-04-29 13:20:09 -0300

    Commit para atualizar campos da classe HostStatus

--- mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/metric/host/HostStatus.java
+++ mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/metric/host/HostStatus.java
@@ -5,11 +5,39 @@ import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.common.Abs
 /**
  * 
  * @author Daivd Araújo - @araujodavid
- * @version 1.0
+ * @version 2.0
  * @since April 28, 2013
  * 
  */
 public class HostStatus extends AbstractMetric {
+
+	private double hostStatusFreeMemory;
+	private int hostStatusInactiveDomains;
+	private int hostStatusActiveDomains;
+	
+	public double getHostStatusFreeMemory() {
+		return hostStatusFreeMemory;
+	}
+
+	public void setHostStatusFreeMemory(double hostStatusFreeMemory) {
+		this.hostStatusFreeMemory = hostStatusFreeMemory;
+	}
+
+	public int getHostStatusInactiveDomains() {
+		return hostStatusInactiveDomains;
+	}
+
+	public void setHostStatusInactiveDomains(int hostStatusInactiveDomains) {
+		this.hostStatusInactiveDomains = hostStatusInactiveDomains;
+	}
+
+	public int getHostStatusActiveDomains() {
+		return hostStatusActiveDomains;
+	}
+
+	public void setHostStatusActiveDomains(int hostStatusActiveDomains) {
+		this.hostStatusActiveDomains = hostStatusActiveDomains;
+	}
 
 	@Override
 	public String toString() {
