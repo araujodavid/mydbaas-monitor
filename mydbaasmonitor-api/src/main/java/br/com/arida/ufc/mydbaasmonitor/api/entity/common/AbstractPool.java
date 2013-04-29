97ef4e5d78749b8f8941faeef7dcf785a711fde0
araujodavid <david.brasillo@gmail.com>
2013-04-28 22:43:16 -0300

    Commit para adicionar a classe abstrata AbstractPool - módulo api

---
+++ mydbaasmonitor-api/src/main/java/br/com/arida/ufc/mydbaasmonitor/api/entity/common/AbstractPool.java
@@ -0,0 +1,25 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.api.entity.common;
+
+import java.util.List;
+
+import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.resource.common.AbstractEntity;
+
+/**
+ * @author Daivd Araújo - @araujodavid
+ * @version 1.0
+ * @since April 28, 2013
+ */
+
+public abstract class AbstractPool<T extends AbstractEntity> {
+	
+	private List<T> pool;
+
+	public List<T> getPool() {
+		return pool;
+	}
+
+	public void setPool(List<T> pool) {
+		this.pool = pool;
+	}
+	
+}
