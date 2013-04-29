bdf7800b3280314bed646145c82b01ba8b147439
araujodavid <david.brasillo@gmail.com>
2013-04-29 15:14:47 -0300

    Commit para adicionar as classes DomainInfo e DomainStatus ao módulo common

---
+++ mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/metric/host/DomainStatus.java
@@ -0,0 +1,19 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.host;
+
+import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.common.AbstractMetric;
+
+/**
+ * 
+ * @author Daivd Araújo - @araujodavid
+ * @version 1.0
+ * @since April 29, 2013
+ * 
+ */
+public class DomainStatus extends AbstractMetric {
+
+	@Override
+	public String toString() {
+		return "domain";
+	}
+
+}
