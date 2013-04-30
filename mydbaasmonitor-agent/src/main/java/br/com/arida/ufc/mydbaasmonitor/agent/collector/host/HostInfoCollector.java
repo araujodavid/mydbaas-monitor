7bed433d691ba9af9eabcb9c39c511410a8a8a88
araujodavid <david.brasillo@gmail.com>
2013-04-30 15:34:30 -0300

    Commit para adicionar os coletores HostInfoCollector e HostStatusCollector - módulo agent

---
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/host/HostInfoCollector.java
@@ -0,0 +1,30 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.host;
+
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.common.AbstractCollector;
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.HostInfoMetric;
+
+/**
+ * 
+ * @author Daivd Araújo - @araujodavid
+ * @version 1.0
+ * @since April 30, 2013
+ * 
+ */
+public class HostInfoCollector extends AbstractCollector<HostInfoMetric> {
+
+	public HostInfoCollector(int identifier) {
+		super(identifier);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void loadMetric(Object[] args) throws Exception {
+		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub		
+	}
+
+}
---
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/host/HostStatusCollector.java
@@ -0,0 +1,30 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.host;
+
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.common.AbstractCollector;
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.HostStatusMetric;
+
+/**
+ * 
+ * @author Daivd Araújo - @araujodavid
+ * @version 1.0
+ * @since April 30, 2013
+ * 
+ */
+public class HostStatusCollector extends AbstractCollector<HostStatusMetric> {
+
+	public HostStatusCollector(int identifier) {
+		super(identifier);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void loadMetric(Object[] args) throws Exception {
+		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub		
+	}
+
+}
