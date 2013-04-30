51d9ef86e7e0edcb9f875a39ad809994b226abf2
araujodavid <david.brasillo@gmail.com>
2013-04-30 14:35:14 -0300

    Commit para atualizar a classe ShellCommand, adicionar os métodos getDomainPid(), getDomainCpuPercentage() e getDomainMemPercentage()

--- mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/util/ShellCommand.java
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/util/ShellCommand.java
@@ -1,13 +1,81 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.agent.util;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * 
  * @author Daivd Araújo - @araujodavid
- * @version 1.0
+ * @version 2.0
  * @since April 29, 2013
  * 
  */
 
 public class ShellCommand {
 	
+	/**
+	 * Method given a domain name is returned your pid
+	 * @param nameDomain
+	 * @return pid process
+	 */
+    public static long getDomainPid(String nameDomain) {
+    	long result = 0;
+        ProcessBuilder process = new ProcessBuilder(new String[]{"bash", "-c", "ps aux | awk '/kvm/ && /"+nameDomain+"/ && !/awk/ {print $2}'"});
+        try {
+            Process p = process.start();
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            //BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String string = "";
+            int l = 0;
+            while ((string = stdInput.readLine()) != null) {
+                if (l == 0 && string != null) {
+                    return Long.parseLong(string);
+                }
+                l++;
+            }
+        } catch (Exception ex) {
+            return result;
+        }
+        return result;
+    }
+    
+    /**
+     * Method given a domain pid is returned your cpu usage
+     * @param domainPid
+     * @return
+     */
+    public static double getDomainCpuPercentage(long domainPid) {
+        double result = 0;
+        ProcessBuilder process = new ProcessBuilder(new String[]{"bash", "-c", "top -p "+domainPid+" -b -n1 | tail -n+8 | sort -nr -k9 | awk '{print $9}'"});
+        try {
+            Process p = process.start();
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            //BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String string = stdInput.readLine();
+            System.out.println(string);
+            return Double.parseDouble(string);
+        } catch (Exception ex) {
+            return result;
+        }
+    }
+    
+    /**
+     * Method given a domain pid is returned your memory usage
+     * @param domainPid
+     * @return
+     */
+    public static double getDomainMemPercentage(long domainPid) {
+        double result = 0;
+        ProcessBuilder process = new ProcessBuilder(new String[]{"bash", "-c", "top -p "+domainPid+" -b -n1 | tail -n+8 | sort -nr -k9 | awk '{print $10}'"});
+        try {
+            Process p = process.start();
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            //BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String string = stdInput.readLine();
+            System.out.println(string);
+            return Double.parseDouble(string);
+        } catch (Exception ex) {
+            return result;
+        }
+    }
 }
