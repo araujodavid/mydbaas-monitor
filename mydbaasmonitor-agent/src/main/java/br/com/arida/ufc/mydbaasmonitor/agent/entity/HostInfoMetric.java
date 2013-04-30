127da0ad98912288a73aa1bcbe3cf7f3e7e2db18
araujodavid <david.brasillo@gmail.com>
2013-04-30 15:00:35 -0300

    Commit para adicionar a classe HostInfoMetric ao módulo agent

---
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/entity/HostInfoMetric.java
@@ -0,0 +1,21 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity;
+
+import java.util.Properties;
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.common.LoadMetric;
+import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.host.HostInfo;
+
+/**
+ * 
+ * @author Daivd Araújo - @araujodavid
+ * @version 1.0
+ * @since April 30, 2013
+ * 
+ */
+public class HostInfoMetric extends HostInfo implements LoadMetric {
+
+	@Override
+	public void loadMetricProperties(Properties properties) {
+		// TODO Auto-generated method stub		
+	}
+
+}
