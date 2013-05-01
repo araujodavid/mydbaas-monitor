7aaf62f18a3427dd08968205d9d79cf93a4b2713
araujodavid <david.brasillo@gmail.com>
2013-05-01 13:29:09 -0300

    Commit para atualizar extends das classes MyDBaaSMonitorConnection e MyDBaaSMonitorDriver

--- mydbaasmonitor-jdbc/src/main/java/br/com/arida/ufc/mydbaasmonitor/jdbc/driver/MyDBaaSMonitorDriver.java
+++ mydbaasmonitor-jdbc/src/main/java/br/com/arida/ufc/mydbaasmonitor/jdbc/driver/MyDBaaSMonitorDriver.java
@@ -1,5 +1,56 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.jdbc.driver;
 
-public class MyDBaaSMonitorDriver {
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class MyDBaaSMonitorDriver implements Driver {
+
+	@Override
+	public boolean acceptsURL(String arg0) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Connection connect(String arg0, Properties arg1) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getMajorVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMinorVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DriverPropertyInfo[] getPropertyInfo(String arg0, Properties arg1)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean jdbcCompliant() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
