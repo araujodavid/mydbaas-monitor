b1e5293cca717b558504c28367330b9a49606d04
araujodavid <david.brasillo@gmail.com>
2013-05-02 11:20:52 -0300

    Adicionar as classes ProcessStatusCollector, DomainInfoCollector e DomainStatusCollector ao módulo agent

---
+++ mydbaasmonitor-agent/src/main/java/br/com/arida/ufc/mydbaasmonitor/agent/collector/host/DomainStatusCollector.java
@@ -0,0 +1,30 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.host;
+
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.collector.common.AbstractCollector;
+import main.java.br.com.arida.ufc.mydbaasmonitor.agent.entity.DomainStatusMetric;
+
+/**
+ * 
+ * @author David Araújo - @araujodavid
+ * @version 1.0
+ * @since May 2, 2013
+ * 
+ */
+public class DomainStatusCollector extends AbstractCollector<DomainStatusMetric> {
+
+	public DomainStatusCollector(int identifier) {
+		super(identifier);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void loadMetric(Object[] args) throws Exception {
+		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub		
+	}
+
+}
