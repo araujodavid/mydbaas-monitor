080210a2ec6228af7487e0708c54f9d47768c34d
araujodavid <david.brasillo@gmail.com>
2013-05-06 15:13:39 -0300

    Atualizar campos da classe Host - módulo common

--- mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/resource/Host.java
+++ mydbaasmonitor-common/src/main/java/br/com/arida/ufc/mydbaasmonitor/common/entity/resource/Host.java
@@ -1,15 +1,37 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.resource;
 
+import java.util.List;
 import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.resource.common.GenericResource;
 
 /**
  * 
  * @author David Araújo - @araujodavid
- * @version 1.0
+ * @version 2.0
  * @since March 22, 2013
  *
  */
 public class Host extends GenericResource {
+
+	//enviroment
+	private DBaaS environment;	
+	//list of virtual machines
+	private List<VirtualMachine> machines;
+	
+	public DBaaS getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(DBaaS environment) {
+		this.environment = environment;
+	}
+	
+	public List<VirtualMachine> getMachines() {
+		return machines;
+	}
+
+	public void setMachines(List<VirtualMachine> machines) {
+		this.machines = machines;
+	}
 
 	@Override
 	public String toString() {
