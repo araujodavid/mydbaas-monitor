49970519edf57e53dedaf9f1a3a59cc79d73c1cf
araujodavid <david.brasillo@gmail.com>
2013-05-06 11:43:07 -0300

    Implementar o método getJsonResult() na classe SendRequest - módulo api

--- mydbaasmonitor-api/src/main/java/br/com/arida/ufc/mydbaasmonitor/api/util/SendResquest.java
+++ mydbaasmonitor-api/src/main/java/br/com/arida/ufc/mydbaasmonitor/api/util/SendResquest.java
@@ -2,13 +2,17 @@ package main.java.br.com.arida.ufc.mydbaasmonitor.api.util;
 
 import java.io.IOException;
 import java.util.List;
+
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 /**
  * 
@@ -28,12 +32,20 @@ public class SendResquest {
 	 * @throws ClientProtocolException
 	 * @throws IOException
 	 */
-	public static HttpResponse postRequest(String url, List<NameValuePair> params, String charset) throws ClientProtocolException, IOException {
+	public static HttpResponse postRequest(String url, List<NameValuePair> params) throws ClientProtocolException, IOException {
 		HttpClient httpClient = new DefaultHttpClient();
-		UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(params, charset);
+		UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(params, "UTF-8");
 		HttpPost httpPost = new HttpPost(url);
 		httpPost.setEntity(formEntity);		
 		HttpResponse response = httpClient.execute(httpPost);
 		return response;
+	}
+	
+	public static String getJsonResult(HttpResponse response) throws IOException {
+		HttpEntity resEntity = response.getEntity();
+		resEntity = new BufferedHttpEntity(resEntity);
+		String result = EntityUtils.toString(resEntity);
+		EntityUtils.toString(resEntity);
+		return result;
 	}
 }
