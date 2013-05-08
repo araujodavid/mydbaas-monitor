5c8115bf654537b79880514c9fd336f755561e76
araujodavid <david.brasillo@gmail.com>
2013-05-08 11:38:50 -0300

    Implementação do método getEntity() e adicionar o método getHostMachines() - HostRepository - módulo core

--- mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/repository/HostRepository.java
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/repository/HostRepository.java
@@ -1,20 +1,30 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.core.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import br.com.caelum.vraptor.ioc.Component;
+import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.resource.DBaaS;
 import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.resource.Host;
+import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.resource.VirtualMachine;
 import main.java.br.com.arida.ufc.mydbaasmonitor.core.repository.common.GenericRepository;
+import main.java.br.com.arida.ufc.mydbaasmonitor.core.util.DataUtil;
 
 /**
  * @author David Araújo - @araujodavid
- * @version 1.0
+ * @version 2.0
  * @since May 8, 2013
  */
 
 @Component
 public class HostRepository implements GenericRepository<Host> {
+	
+	private Connection connection = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+    private VirtualMachineRepository virtualMachineRepository;
 
 	@Override
 	public List<Host> all() {
@@ -45,11 +55,28 @@ public class HostRepository implements GenericRepository<Host> {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public List<VirtualMachine> getHostMachines(Host host) {
+		
+		return null;
+	}
 
 	@Override
 	public Host getEntity(ResultSet resultSet) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Host host = new Host();
+		DBaaS dBaaS = new DBaaS();
+		host.setId(resultSet.getInt("id"));
+		host.setRecordDate(DataUtil.converteDateParaString(resultSet.getDate("record_date")));
+		host.setAlias(resultSet.getString("alias"));
+		host.setDescription(resultSet.getString("description"));
+		host.setHost(resultSet.getString("host"));
+		host.setPort(resultSet.getInt("port"));
+		host.setUser(resultSet.getString("username"));
+		host.setPassword(resultSet.getString("password"));
+		host.setStatus(resultSet.getBoolean("status"));
+		dBaaS.setId(resultSet.getInt("dbaas"));
+		host.setEnvironment(dBaaS);
+		return host;
 	}
 
 }
