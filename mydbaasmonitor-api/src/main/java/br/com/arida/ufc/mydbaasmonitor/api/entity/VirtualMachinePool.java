baa172b0118ee74b968ebcbbeac661ad22e2c250
araujodavid <david.brasillo@gmail.com>
2013-05-10 12:51:59 -0300

    Implementação do método getDBMSs - VirtualMachinePool - módulo api

--- mydbaasmonitor-api/src/main/java/br/com/arida/ufc/mydbaasmonitor/api/entity/VirtualMachinePool.java
+++ mydbaasmonitor-api/src/main/java/br/com/arida/ufc/mydbaasmonitor/api/entity/VirtualMachinePool.java
@@ -1,14 +1,22 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.api.entity;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.message.BasicNameValuePair;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import main.java.br.com.arida.ufc.mydbaasmonitor.api.entity.common.AbstractPool;
+import main.java.br.com.arida.ufc.mydbaasmonitor.api.util.SendResquest;
 import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.resource.DBMS;
 import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.resource.VirtualMachine;
 
 /**
  * @author Daivd Araújo
- * @version 1.0
+ * @version 2.0
  * @since April 1, 2013
  */
 
@@ -26,9 +34,28 @@ public class VirtualMachinePool extends AbstractPool<VirtualMachine> {
 		return false;
 	}
 	
-	public List<DBMS> getDBMSs(int virtualMachineId) {
-		//TODO
-		return null;
+	/**
+	 * Method to retrieve the dbmss of a particular Virtual Machine
+	 * @param resource- Virtual Machine object owner
+	 * @return a list of dbmss of the Virtual Machine
+	 */
+	public List<DBMS> getDBMSs(VirtualMachine resource) {
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("identifier", String.valueOf(resource.getId())));
+		params.add(new BasicNameValuePair("ownerType", resource.toString()));
+		HttpResponse response;
+		String json = null;
+		try {
+			response = SendResquest.postRequest(this.getClient().getServerUrl()+"/resource/dbmss", params);
+			json = SendResquest.getJsonResult(response);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Gson gson = new Gson();
+		List<DBMS> dbmss = gson.fromJson(json, new TypeToken<List<DBMS>>(){}.getType());
+		return dbmss;
 	}
 
 }
