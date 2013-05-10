058ec5c7de0c1e54d1b18965aa0525abf0b745ad
araujodavid <david.brasillo@gmail.com>
2013-05-10 10:22:33 -0300

    Adicionar classe MetricController, gerenciar requisições da API - módulo core

---
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/api/MetricController.java
@@ -0,0 +1,17 @@
+package main.java.br.com.arida.ufc.mydbaasmonitor.core.controller.api;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+
+/**
+ * Class that handles metrics requests sent by the API module.
+ * @author Daivd Araújo - @araujodavid
+ * @version 1.0
+ * @since May 9, 2013 
+ */
+
+@Resource
+@Path("/metric")
+public class MetricController {
+
+}
