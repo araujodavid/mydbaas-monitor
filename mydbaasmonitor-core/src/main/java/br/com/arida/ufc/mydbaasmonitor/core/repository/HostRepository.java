175c92a69d3571eb89ba511dac0b709f3d657b4a
araujodavid <david.brasillo@gmail.com>
2013-05-15 00:39:37 -0300

    Atualizar o método getEntity() e adicionar o método updateHostInformation() - HostRepository - módulo core

--- mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/repository/HostRepository.java
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/repository/HostRepository.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import br.com.caelum.vraptor.ioc.Component;
+import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.host.HostInfo;
 import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.resource.DBaaS;
 import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.resource.Host;
 import main.java.br.com.arida.ufc.mydbaasmonitor.core.repository.common.GenericRepository;
@@ -15,7 +16,7 @@ import main.java.br.com.arida.ufc.mydbaasmonitor.core.util.DataUtil;
 
 /**
  * @author David Araújo - @araujodavid
- * @version 3.0
+ * @version 4.0
  * @since May 8, 2013
  */
 
@@ -183,9 +184,41 @@ public class HostRepository implements GenericRepository<Host> {
 		}
 	}//updatePassword()
 	
+	public boolean updateHostInformation(HostInfo hostInfo, int hostId) {
+		try {
+			this.connection = Pool.getConnection(Pool.JDBC_MySQL);
+			this.preparedStatement = this.connection.prepareStatement(
+					"update `host` " +
+					"set `host_name` = ?, `host_hypervisor` = ?, `host_cores` = ?, `host_cpus` = ?, `host_memory` = ?, `host_mhz` = ?, " +
+					"`host_model` = ? " +
+					"where `id` = ?;");
+			
+			this.preparedStatement.setString(1, hostInfo.getHostInfoName());
+			this.preparedStatement.setString(2, hostInfo.getHostInfoHypervisor());
+			this.preparedStatement.setInt(3, hostInfo.getHostInfoCores());
+			this.preparedStatement.setInt(4, hostInfo.getHostInfoCpus());
+			this.preparedStatement.setDouble(5, hostInfo.getHostInfoMemory());
+			this.preparedStatement.setDouble(6, hostInfo.getHostInfoMhz());
+			this.preparedStatement.setString(7, hostInfo.getHostInfoModel());
+			this.preparedStatement.setInt(8, hostId);
+			
+			this.preparedStatement.executeUpdate();
+			return true;
+		}
+		catch(SQLException se) {se.printStackTrace();}
+		catch (RuntimeException re) {re.printStackTrace();}
+		finally {
+			try { resultSet.close(); } catch(Exception e) { }
+            try { preparedStatement.close(); } catch(Exception e) { }
+            try { connection.close(); } catch(Exception e) { }
+		}
+		return false;
+	}//updateHostInformation()
+	
 	@Override
 	public Host getEntity(ResultSet resultSet) throws SQLException {
 		Host host = new Host();
+		HostInfo hostInfo = new HostInfo();
 		DBaaS dBaaS = new DBaaS();
 		host.setId(resultSet.getInt("id"));
 		host.setRecordDate(DataUtil.converteDateParaString(resultSet.getDate("record_date")));
@@ -197,7 +230,15 @@ public class HostRepository implements GenericRepository<Host> {
 		host.setPassword(resultSet.getString("password"));
 		host.setStatus(resultSet.getBoolean("status"));
 		dBaaS.setId(resultSet.getInt("dbaas"));
+		hostInfo.setHostInfoName(resultSet.getString("host_name"));
+		hostInfo.setHostInfoHypervisor(resultSet.getString("host_hypervisor"));
+		hostInfo.setHostInfoCores(resultSet.getInt("host_cores"));
+		hostInfo.setHostInfoCpus(resultSet.getInt("host_cpus"));
+		hostInfo.setHostInfoMemory(resultSet.getDouble("host_memory"));
+		hostInfo.setHostInfoMhz(resultSet.getDouble("host_mhz"));
+		hostInfo.setHostInfoModel(resultSet.getString("host_model"));
 		host.setEnvironment(dBaaS);
+		host.setInformation(hostInfo);
 		return host;
 	}
 
