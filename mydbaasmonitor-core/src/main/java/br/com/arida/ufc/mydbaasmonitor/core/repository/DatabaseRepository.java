480f20b999076d5cef21c3bfabc284b6f8ed6e53
araujodavid <david.brasillo@gmail.com>
2013-05-30 22:58:17 -0300

    Implementar a classe DatabaseRepository - módulo core

--- mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/repository/DatabaseRepository.java
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/repository/DatabaseRepository.java
@@ -28,9 +28,26 @@ public class DatabaseRepository implements GenericRepository<Database> {
     
 	@Override
 	public List<Database> all() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		ArrayList<Database> databaseList = new ArrayList<Database>();
+		try {
+			connection = Pool.getConnection(Pool.JDBC_MySQL);
+			preparedStatement = connection.prepareStatement("select * from `database` order by `id`;");
+						
+			resultSet = preparedStatement.executeQuery();
+			while(resultSet.next()){
+				Database database = getEntity(resultSet);
+				databaseList.add(database);
+			}
+		}
+		catch(SQLException se) {se.printStackTrace();}
+		catch (RuntimeException re) {re.printStackTrace();}
+		finally {
+            try { resultSet.close(); } catch(Exception e) {}
+            try { preparedStatement.close(); } catch(Exception e) {}
+            try { connection.close(); } catch(Exception e) {}
+        }		
+		return databaseList;
+	}//all()
 	
 	public List<Database> getDBMSDatabases(int dbmsId) {
 		ArrayList<Database> databases = new ArrayList<Database>();
@@ -59,9 +76,27 @@ public class DatabaseRepository implements GenericRepository<Database> {
 	
 	@Override
 	public Database find(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		Database database = null;
+		try {
+			connection = Pool.getConnection(Pool.JDBC_MySQL);
+			preparedStatement = connection.prepareStatement("select * from `database` where `id` = ?;");
+			
+			preparedStatement.setInt(1, id);
+			
+			resultSet = preparedStatement.executeQuery();
+			while(resultSet.next()){
+				database = getEntity(resultSet);	
+			}
+		}
+		catch(SQLException se) {se.printStackTrace();}
+		catch (RuntimeException re) {re.printStackTrace();}
+		finally {
+            try { resultSet.close(); } catch(Exception e) {}
+            try { preparedStatement.close(); } catch(Exception e) {}
+            try { connection.close(); } catch(Exception e) {}
+        }
+		return database;
+	}//find()
 	
 	@Override
 	public void remove(Database resource) {
@@ -71,15 +106,55 @@ public class DatabaseRepository implements GenericRepository<Database> {
 	
 	@Override
 	public void save(Database resource) {
-		// TODO Auto-generated method stub
-		
-	}
+		try {
+			this.connection = Pool.getConnection(Pool.JDBC_MySQL);
+			this.preparedStatement = this.connection.prepareStatement(
+					"insert into `database` " +
+					"(`alias`, `record_date`, `description`, `name`, `status`, `dbms`) " +
+					"values (?, now(), ?, ?, false, ?);");
+			
+			this.preparedStatement.setString(1, resource.getAlias());
+			this.preparedStatement.setString(2, resource.getDescription());
+			this.preparedStatement.setString(3, resource.getName());
+			this.preparedStatement.setInt(4, resource.getDbms().getId());
+			
+			this.preparedStatement.executeUpdate();
+		} 
+		catch(SQLException se) {se.printStackTrace();}
+		catch (RuntimeException re) {re.printStackTrace();}
+		finally {
+            try { resultSet.close(); } catch(Exception e) { }
+            try { preparedStatement.close(); } catch(Exception e) { }
+            try { connection.close(); } catch(Exception e) { }
+        }
+	}//save()
 	
 	@Override
 	public void update(Database resource) {
-		// TODO Auto-generated method stub
-		
-	}
+		try {
+			this.connection = Pool.getConnection(Pool.JDBC_MySQL);
+			this.preparedStatement = this.connection.prepareStatement(
+					"update `database` " +
+					"set `alias` = ?, `description` = ?, `name` = ?, `status` = ?, `dbms` = ? " +
+					"where `id` = ?;");
+			
+			this.preparedStatement.setString(1, resource.getAlias());
+			this.preparedStatement.setString(2, resource.getDescription());
+			this.preparedStatement.setString(3, resource.getName());
+			this.preparedStatement.setBoolean(4, resource.getStatus());
+			this.preparedStatement.setInt(5, resource.getDbms().getId());
+			this.preparedStatement.setInt(6, resource.getId());
+			
+			this.preparedStatement.executeUpdate();
+		}
+		catch(SQLException se) {se.printStackTrace();}
+		catch (RuntimeException re) {re.printStackTrace();}
+		finally {
+			try { resultSet.close(); } catch(Exception e) { }
+            try { preparedStatement.close(); } catch(Exception e) { }
+            try { connection.close(); } catch(Exception e) { }
+		}
+	}//update()
 	
 	@Override
 	public Database getEntity(ResultSet resultSet) throws SQLException {
@@ -90,6 +165,7 @@ public class DatabaseRepository implements GenericRepository<Database> {
 		database.setRecordDate(DataUtil.converteDateParaString(resultSet.getTimestamp("record_date")));
 		database.setDescription(resultSet.getString("description"));
 		database.setName(resultSet.getString("name"));
+		database.setStatus(resultSet.getBoolean("status"));
 		dbms.setId(resultSet.getInt("dbms"));
 		database.setDbms(dbms);
 		return database;
