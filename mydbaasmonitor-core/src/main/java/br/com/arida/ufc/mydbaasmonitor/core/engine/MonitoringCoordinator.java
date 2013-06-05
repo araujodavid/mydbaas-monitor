885763c4585c7716d4e5aeb266f3ce097d5bd59f
araujodavid <david.brasillo@gmail.com>
2013-06-04 21:44:54 -0300

    Atualizar comentários da classe MonitoringCoordinator - módulo core

--- mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/engine/MonitoringCoordinator.java
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/engine/MonitoringCoordinator.java
@@ -1,11 +1,13 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.core.engine;
 
 /**
- * @author David Araújo
- *
+ * Class that manages the methods to configure and send agent to resources.
+ * @author David Araújo - @araujodavid
+ * @version 1.0
+ * @since June 3, 2013
  */
 
 public class MonitoringCoordinator {
-
+	
 	
 }
