65b1c8313b8ff38f0e3737591e16faee61efc0d8
araujodavid <david.brasillo@gmail.com>
2013-06-06 16:04:14 -0300

    Implementar os métodos para receber as requisições sobre métricas - MetricController - módulo core

--- mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/api/MetricController.java
+++ mydbaasmonitor-core/src/main/java/br/com/arida/ufc/mydbaasmonitor/core/controller/api/MetricController.java
@@ -2,7 +2,6 @@ package main.java.br.com.arida.ufc.mydbaasmonitor.core.controller.api;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
 import main.java.br.com.arida.ufc.mydbaasmonitor.core.repository.MetricRepository;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
@@ -12,7 +11,7 @@ import br.com.caelum.vraptor.view.Results;
 /**
  * Class that handles metrics requests sent by the API module.
  * @author Daivd Araújo - @araujodavid
- * @version 1.0
+ * @version 2.0
  * @since May 9, 2013 
  */
 
@@ -28,6 +27,16 @@ public class MetricController {
 		this.metricRepository = metricRepository;
 	}
 	
+	/**
+	 * Method to requests about the last collection of a particular metric
+	 * @param metricName - Metric name
+	 * @param resourceType - Resource type
+	 * @param metricType - Metric type
+	 * @param resourceID - Resource ID
+	 * @param startDatetime - Start time
+	 * @param endDatetime - End time
+	 * Return json of the metric
+	 */
 	@Path("/single")
 	public void getMetricSingle(String metricName, String resourceType, int metricType, int resourceID, String startDatetime, String endDatetime) {
 		Class<?> metricClass = null;
@@ -47,7 +56,6 @@ public class MetricController {
 			try {			
 				sql = this.metricRepository.makeQuerySQL(metricClass, metricType, resourceID, MetricRepository.LAST_COLLECTION, startDatetime, endDatetime);
 				List<Object> metric = this.metricRepository.queryMetrics(sql, metricClass);
-				
 				result
 				.use(Results.json())
 				.withoutRoot()
@@ -84,9 +92,48 @@ public class MetricController {
 		}		
 	}
 	
+	/**
+	 * Method to requests collections of a particular metric
+	 * @param metricName - Metric name
+	 * @param resourceType - Resource type
+	 * @param metricType - Metric type
+	 * @param resourceID - Resource ID
+	 * @param startDatetime - Start time
+	 * @param endDatetime - End time
+	 * Return a json of the metric list
+	 */
 	@Path("/multi")
-	public void getMetricMulti(String metricName, int resourceID, String resourceType, int queryType, String startDatetime, String endDatetime) {
-		//TODO		
+	public void getMetricCollection(String metricName, String resourceType, int resourceID, String startDatetime, String endDatetime) {
+		Class<?> metricClass = null;
+		String sql = null;
+		
+		try {
+			if (resourceType.equals("dbms") || resourceType.equals("database")) {			
+				metricClass = Class.forName("main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.database."+metricName);						
+			} else {
+				metricClass = Class.forName("main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric."+resourceType+"."+metricName);
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {			
+			sql = this.metricRepository.makeQuerySQL(metricClass, MetricRepository.METRIC_NO_TYPE, resourceID, MetricRepository.ALL_COLLECTION, startDatetime, endDatetime);
+			List<Object> metric = this.metricRepository.queryMetrics(sql, metricClass);
+			result
+			.use(Results.json())
+			.withoutRoot()
+			.from(metric)
+			.serialize();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
