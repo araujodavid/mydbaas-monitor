2727e0ec6feed6ab71cee9fdb88df271625e2ee4
araujodavid <david.brasillo@gmail.com>
2013-06-06 16:18:30 -0300

    Atualizar JAR common - módulo api

--- mydbaasmonitor-api/src/main/java/br/com/arida/ufc/mydbaasmonitor/api/util/Test.java
+++ mydbaasmonitor-api/src/main/java/br/com/arida/ufc/mydbaasmonitor/api/util/Test.java
@@ -1,12 +1,8 @@
 package main.java.br.com.arida.ufc.mydbaasmonitor.api.util;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.message.BasicNameValuePair;
+import main.java.br.com.arida.ufc.mydbaasmonitor.api.client.MyDBaaSMonitorClient;
+import main.java.br.com.arida.ufc.mydbaasmonitor.api.client.MyMetrics;
+import main.java.br.com.arida.ufc.mydbaasmonitor.common.entity.metric.machine.Cpu;
 
 public class Test {
 
@@ -14,36 +10,45 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//		MyDBaaSMonitorClient client = new MyDBaaSMonitorClient("http://localhost:8080/mydbaasmonitor");
-//		DBaaSPool pool = client.getMyDBaaSs();
-//		for (DBaaS dBaaS : pool.getPool()) {
-//			System.out.println(dBaaS.getAlias());
-//			System.out.println(dBaaS.getRecordDate());
-//			System.out.println(dBaaS.getId());
-//			System.out.println(dBaaS.getDescription());
-//			for (VirtualMachine machine : dBaaS.getMachines()) {
-//				System.out.println(machine.getHost());
-//				System.out.println(machine.getAlias());
-//			}
-//		}
+		MyDBaaSMonitorClient client = new MyDBaaSMonitorClient("http://localhost:8080/mydbaasmonitor");
+		MyMetrics myMetrics = new MyMetrics(client);
+//		Memory memory = (Memory) myMetrics.getMetricSingle("Memory", "machine", 5, null, null);
 //		
-		List<NameValuePair> parameters = new ArrayList<NameValuePair>();
-		parameters.add(new BasicNameValuePair("metricName", "Memory"));
-		parameters.add(new BasicNameValuePair("resourceType", "machine"));
-		parameters.add(new BasicNameValuePair("metricType", "1"));
-		parameters.add(new BasicNameValuePair("resourceID", "3"));
-		
-		try {
-			HttpResponse response = SendResquest.postRequest("http://10.41.18.148:8080/mydbaasmonitor/metric/single", parameters);
-			String string = SendResquest.getJsonResult(response);
-			System.out.println(string);
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+//		System.out.println(memory.getRecordDate());
+//		System.out.println(memory.getMemoryUsedPercent());
+//		System.out.println(memory.getMemoryUsed());
+//		System.out.println(memory.getMemorySwapUsedPercent());
+//		System.out.println(memory.getMemorySwapUsed());
+//		System.out.println(memory.getMemorySwapFreePercent());
+//		System.out.println(memory.getMemorySwapFree());
+//		System.out.println(memory.getMemoryFreePercent());
+//		System.out.println(memory.getMemoryFree());
+//		System.out.println(memory.getMemoryBuffersCacheUsed());
+//		System.out.println(memory.getMemoryBuffersCacheFree());
+//		
+		for (Object metric : myMetrics.getMetricMulti("Cpu", "machine", 5, null, null)) {
+			Cpu cpu = (Cpu) metric;
+			System.out.println(cpu.getCpuCombined());
 		}
 		
-		
+//		List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+//		parameters.add(new BasicNameValuePair("metricName", "Cpu"));
+//		parameters.add(new BasicNameValuePair("resourceType", "machine"));
+//		parameters.add(new BasicNameValuePair("metricType", "1"));
+//		parameters.add(new BasicNameValuePair("resourceID", "5"));
+//		parameters.add(new BasicNameValuePair("startDatetime", "05-06-2013 19:26:25"));
+//		parameters.add(new BasicNameValuePair("endDatetime", "05-06-2013 19:26:35"));
+//		
+//		try {
+//			HttpResponse response = SendResquest.postRequest("http://localhost:8080/mydbaasmonitor/metric/multi", parameters);
+//			String string = SendResquest.getJsonResult(response);
+//			System.out.println(string);
+//		} catch (ClientProtocolException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+				
 //		for (Method method : VirtualMachine.class.getDeclaredMethods()) {
 //			if (method.getName().startsWith("get") && !method.getReturnType().getName().equals("java.util.List")) {
 //				System.out.println(method.getName());
